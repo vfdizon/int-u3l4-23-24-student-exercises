4541bc1f0c3aa87eacbc5266cc6c6734ac62c491
vfdizon <vdizon@cps.edu>
2023-10-30 16:10:33 +0000

    completed all tasks

--- domManipulation.js
+++ domManipulation.js
@@ -7,8 +7,17 @@ let image2Button = document.querySelector('.image-2-button');
 let para = document.querySelector('.paragraph');
 let image = document.querySelector('.image');
 
+let userName = document.querySelector(".name");
+let hobby = document.querySelector(".hobby");
+let home = document.querySelector(".location");
+
 // CODE ALONG
 // 1. An event listener in provided for the `contentButton`
+contentButton.addEventListener('click', function() {
+	userName.innerHTML = "My name is Vincent";
+	hobby.innerHTML = "I like to play music";
+	home.innerHTML = "I live in Chicago";
+})
 // 2. When clicked, it should change the content of:
 //   - The `name` header.
 
@@ -18,23 +27,20 @@ let image = document.querySelector('.image');
 //   - The `hobby` paragraph.
 //   - The `location` paragraph.
 
-contentButton.addEventListener('click', function() {
-
-
-
-
-
-
-});
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
-
+image1Button.addEventListener('click', function() {
+	image.src = "goya.jpg";
+});
 
 
 
 
 // 5. Add an event listener to the `image2Button`
+image2Button.addEventListener('click', function(){
+	image.src = "orange.jpg";
+});
 // 6. When clicked, it should change the source of `image` to another image URL.
 
 
--- script.js
+++ script.js
@@ -4,15 +4,17 @@ let img = document.querySelector("img");
 
 // 1. Press the button on the webpage to change it up! Take notice of what happens.
 // - What do you think innerHTML does?
+// change the content 
 // - What do you think .src does?
+// change the source of the image
 
 
 // 2. Try the following:
 // - Make it so that the header says your name when the button is clicked.
 // - Change the src of the image to something random.
 button.addEventListener("click", function() {
-    header.innerHTML = "🤯 wow cool neeto";
-    img.src = "https://media4.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif";
+    header.innerHTML = "vincent";
+    img.src = "hampster.jpg";
 });
 
 
